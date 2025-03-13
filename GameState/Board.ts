0673c39f2c232bd843ddc32523df1f2bac1516d9
aayushkt <aayushkt@gmail.com>
2025-03-12 21:49:24 -0400

    begin work on vertices and neighbors

--- GameState/Board.ts
+++ GameState/Board.ts
@@ -5,87 +5,97 @@ export class Harbor {
 }
 
 export class Board {
+
   harbors: { [Location: number] : Harbor };
   tileValues: number[];
   tileResources: Resource[];
 
   constructor() {
-    this.generateTileResourcesRandomly();
-    this.generateTileValuesAccordingToAlphabet();
+    this.tileResources = this.generateTileResourcesRandomly();
+    this.tileValues = this.generateTileValuesAccordingToAlphabet(this.tileResources);
   }
 
-  private generateTileResourcesRandomly() {
+  private generateTileResourcesRandomly(): Resource[] {
 
-    this.tileResources = [];
+    let tileResources: Resource[] = [];
 
     // default catan board contains:
 
     // 3 bricks
-    this.tileResources.push(Resource.Brick);
-    this.tileResources.push(Resource.Brick);
-    this.tileResources.push(Resource.Brick);
+    tileResources.push(Resource.Brick);
+    tileResources.push(Resource.Brick);
+    tileResources.push(Resource.Brick);
 
     // 4 lumber
-    this.tileResources.push(Resource.Lumber);
-    this.tileResources.push(Resource.Lumber);
-    this.tileResources.push(Resource.Lumber);
-    this.tileResources.push(Resource.Lumber);
+    tileResources.push(Resource.Lumber);
+    tileResources.push(Resource.Lumber);
+    tileResources.push(Resource.Lumber);
+    tileResources.push(Resource.Lumber);
 
     // 3 ore
-    this.tileResources.push(Resource.Ore);
-    this.tileResources.push(Resource.Ore);
-    this.tileResources.push(Resource.Ore);
+    tileResources.push(Resource.Ore);
+    tileResources.push(Resource.Ore);
+    tileResources.push(Resource.Ore);
 
     // 4 grain
-    this.tileResources.push(Resource.Grain);
-    this.tileResources.push(Resource.Grain);
-    this.tileResources.push(Resource.Grain);
-    this.tileResources.push(Resource.Grain);
+    tileResources.push(Resource.Grain);
+    tileResources.push(Resource.Grain);
+    tileResources.push(Resource.Grain);
+    tileResources.push(Resource.Grain);
 
     // 4 wool
-    this.tileResources.push(Resource.Wool);
-    this.tileResources.push(Resource.Wool);
-    this.tileResources.push(Resource.Wool);
-    this.tileResources.push(Resource.Wool);
+    tileResources.push(Resource.Wool);
+    tileResources.push(Resource.Wool);
+    tileResources.push(Resource.Wool);
+    tileResources.push(Resource.Wool);
 
     // and 1 desert
-    this.tileResources.push(Resource.Desert);
+    tileResources.push(Resource.Desert);
 
-    // shuffle them all up and place them
-    shuffle(this.tileResources);
+    // shuffle them all up 
+    shuffle(tileResources);
+
+    return tileResources;
   }
 
-  private generateTileValuesAccordingToAlphabet() {
+  private generateTileValuesAccordingToAlphabet(tileResources: Resource[]): number[] {
+    let tileValues: number[] = [];
     // note: this is the values in their (reverse) alphabetical-spiral order 
     // that corresponds to the official rule book (not the "Variable Setup")
-    this.tileValues = [];
     let orderedValues = [11, 3, 6, 5, 4, 9, 10, 8, 4, 11, 12, 9, 10, 8, 3, 6, 2, 5];
-    for (let i = 0; i < 19; ++i) {
-      if (this.tileResources[i] == Resource.Desert) {
-        this.tileValues.push(0);
+    for (let i = 0; i < tileResources.length; ++i) {
+      if (tileResources[i] == Resource.Desert) {
+        tileValues.push(0);
         continue;
       }
       const temp = orderedValues.pop();
       if (temp == undefined) {
-        this.tileValues.push(0);
+        tileValues.push(0);
       } else {
-        this.tileValues.push(temp);
+        tileValues.push(temp);
       }
     }
+
+    return tileValues;
   }
 
-  private generateTileValuesRandomly() {
-    let allValues = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12];
-    shuffle(allValues);
-    // for (let i = 0; i < 19; ++i) {
-    //   if (this.tileResources[i] == Resource.Desert) {
-    //     this.tileValues.push(0);
-    //   } else {
-    //     this.tileValues.push(allValues.pop());
-    //   }
-    // }
+  public GetNeighborsOfVertex(vertex: number): number[] {
+    if (vertex < 0 || vertex > 53) return [];
 
-    // TODO: no two red numbers (6s or 8s) should be adjacent
+    // see CatanBoardVerticesAnnotated.png for a clear diagram
+    // on how this algorithm works and to understand the terms used here
+
+    // first, find out the row number of the vertex
+    
+    // then find the position in the row (the column)
+
+    // its three neighbors will be on the other side of the row (upper/lower)
+    // in the same position and position + 1
+
+    // also check for edge cases (if the vertex is on the left edge it wont)
+    // have three neighbors
+
+    return [];
   }
 
 }
