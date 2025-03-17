1c2af1beb7f61a76abfe62989813dbf1c05927c2
aayushkt <aayushkt@gmail.com>
2025-03-17 11:51:35 -0400

    added dice roll action

--- src/GameExecution/Action.ts
+++ src/GameExecution/Action.ts
@@ -11,3 +11,18 @@ export abstract class Action {
     protected abstract RunAction(gameState: GameState): void;
     
 }
+
+export function RollDice(gamestate: GameState) {
+    for (let playerIndex = 0; playerIndex < gamestate.players.length; ++playerIndex) {
+        const player = gamestate.players[playerIndex];
+        for (let settlementIndex = 0; settlementIndex < player.settlements.length; ++settlementIndex) {
+            const settlement = player.settlements[settlementIndex];
+            const tilesForSettlement = gamestate.board.GetTilesOfVertex(settlement);
+            for (let tileIndex = 0; tileIndex < tilesForSettlement.length; ++tileIndex) {
+                const tile = tilesForSettlement[tileIndex];
+                const resource = gamestate.board.tileResources[tile];
+                player.resources[resource] += gamestate.board.RollProbability(gamestate.board.tileValues[tile]);
+            }
+        }
+    }
+}
--- src/GameState/Board.ts
+++ src/GameState/Board.ts
@@ -244,6 +244,23 @@ export class Board {
     }
   }
 
+  public RollProbability(tileValue: number): number {
+    switch (tileValue) {
+      case 2: return 1/36;
+      case 3: return 2/36;
+      case 4: return 3/36;
+      case 5: return 4/36;
+      case 6: return 5/36;
+      case 7: return 0;
+      case 8: return 5/36;
+      case 9: return 4/36;
+      case 10: return 3/36;
+      case 11: return 2/36;
+      case 12: return 1/36 ;
+      default: throw `ERROR: It is impossible to roll a ${tileValue} with two D6's!`;
+    }
+  }
+
 }
 
 export enum Resource {
